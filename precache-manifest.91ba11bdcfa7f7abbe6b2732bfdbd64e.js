b894a3942df153581c4f880de72e0a152dce3bb0
David Codina <davidmcodina@hotmail.com>
2019-10-08 21:22:16 -0600

    Updates

---
+++ precache-manifest.91ba11bdcfa7f7abbe6b2732bfdbd64e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6457ad69bd353f28b268e29cd2ce708",
+    "url": "/react-photowall/index.html"
+  },
+  {
+    "revision": "65217c2a2288488b86d7",
+    "url": "/react-photowall/static/css/main.a4bcf1f8.chunk.css"
+  },
+  {
+    "revision": "474b46dbe15f32ab8789",
+    "url": "/react-photowall/static/js/2.d9237176.chunk.js"
+  },
+  {
+    "revision": "65217c2a2288488b86d7",
+    "url": "/react-photowall/static/js/main.d55047f3.chunk.js"
+  },
+  {
+    "revision": "f4e44f13cd190a1c8430",
+    "url": "/react-photowall/static/js/runtime-main.2541501f.js"
+  },
+  {
+    "revision": "65ee16278d11cf98e01efdf0b58d78d8",
+    "url": "/react-photowall/static/media/billabong-webfont.65ee1627.woff"
+  },
+  {
+    "revision": "a78aec854f75c6b47279e9dba903d86f",
+    "url": "/react-photowall/static/media/speech-bubble-blue.a78aec85.svg"
+  },
+  {
+    "revision": "5bdca9ea86f047a9eafdcef9e116d59d",
+    "url": "/react-photowall/static/media/speech-bubble-white.5bdca9ea.svg"
+  }
+]);
